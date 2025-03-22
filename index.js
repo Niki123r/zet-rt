ee95dd397daff3bfb53b6474c14e3de404dc40b4
Niki123r <nikola.lipnoj@gmail.com>
2025-03-22 23:00:37 +0100

    Change default port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { calculateBearing } = require("./utils.js");
 const fs = require("fs");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 app.use(express.static("public"));
 
 const url = "https://www.zet.hr/gtfs-rt-protobuf";
